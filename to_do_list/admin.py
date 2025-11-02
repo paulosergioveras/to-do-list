7a5ae0dc79b8f0c735e41d47144c1096ab29b58c
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-11-02 18:22:20 -0300

    feat: create admin model

--- to_do_list/admin.py
+++ to_do_list/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+
+@admin.register(Todo)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'completed',
+        'priority',
+        'due_data',
+        'created_at',
+        'updated_at'
+    )
